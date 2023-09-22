a7ab4d86ea24aaa0b3d4f5d9f78a88239c6a306f
Viktor3RDart <fable198666@mail.ru>
2023-09-22 20:07:19 +0300

    selenide done

---
+++ src/test/java/test/PobedaSearchOrderPage.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class PobedaSearchOrderPage {
+
+    private SelenideElement errorMessage = $("div.message_error");
+
+    @Step("Переключится на вкладку № {numberWindow}")
+    public void goToNextTab(int numberWindow) {
+        switchTo().window(numberWindow);
+    }
+
+    public SelenideElement getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(SelenideElement errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
